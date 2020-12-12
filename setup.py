4f0ded490b4057c8380df3c27e6c0d7e54502a83
refraction-ray <kcanamgal@foxmail.com>
2020-12-12 14:19:40 +0800

    version0.11.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ for f in excludes:
 
 setuptools.setup(
     name="xalpha",
-    version="0.11.0",
+    version="0.11.1",
     author="refraction-ray",
     author_email="refraction-ray@protonmail.com",
     description="all about fund investment",
--- xalpha/__init__.py
+++ xalpha/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.11.0"
+__version__ = "0.11.1"
 __author__ = "refraction-ray"
 __name__ = "xalpha"
 
