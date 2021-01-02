79ee5c00129eae52851160a9c9b3ecef6ea96953
refraction-ray <kcanamgal@foxmail.com>
2021-01-02 19:36:40 +0800

    shengou*100 in the first trade

--- xalpha/trade.py
+++ xalpha/trade.py
@@ -380,7 +380,7 @@ class trade:
                     feelabel = None
                 value = int(value * 100) / 100
                 assert feelabel is None or feelabel >= 0.0, "自定义申购费必须为正值"
-                rdate, cash, share = self.aim.shengou(value, date, fee=feelabel)
+                rdate, cash, share = self.aim.shengou(value, date, fee=feelabel * 100)
                 rem = rm.buy([], share, rdate)
             else:
                 raise TradeBehaviorError("You cannot sell first when you never buy")
@@ -441,6 +441,7 @@ class trade:
 
                 if value > 0:  # value stands for purchase money
                     feelabel = 100 * value - int(100 * value)
+
                     if int(10 * feelabel) == 5:
                         feelabel = (feelabel - 0.5) * 100
                     else:
